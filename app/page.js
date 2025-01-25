0cc4d5d4ad2a99bb09ae26ad60b6512c369b989f
Wynter Jones <34955844+WynterJones@users.noreply.github.com>
2025-01-24 20:29:35 -0500

    first commit

--- app/page.js
+++ app/page.js
@@ -1,101 +1,114 @@
 import Image from "next/image";
+import Link from "next/link";
+
+const products = [
+  {
+    title: "APIFunnels",
+    description:
+      "Use the ClickFunnels 2.0 API in your landing pages safely without exposing your API key. For marketing ninjas who want to do more.",
+    image: "/apifunnels.png",
+    link: "https://apifunnels.com",
+  },
+  {
+    title: "SurveyPeek",
+    description:
+      "Track, import, filter and export your ClickFunnels 2.0 survey data with ease. Made for any one running a survey in ClickFunnels.",
+    image: "/surveypeek.png",
+    link: "https://surveypeek.com",
+  },
+  {
+    title: "CFAlerts",
+    description:
+      "A mobile app to make your phone go cha-ching every time you make a sale or lead in ClickFunnels.",
+    image: "/comingsoon.png",
+    link: null,
+  },
+  {
+    title: "SnailTrain",
+    description:
+      "A Ruby on Rails 8 scaffolding generator with AI super powers. For developers who love to code.",
+    image: "/snailtrain.png",
+    link: "http://snailtrain.com",
+  },
+  {
+    title: "CustomLogin",
+    description:
+      "Add custom login pages for your ClickFunnels 2.0 communities and memberships with ease.",
+    image: "/comingsoon.png",
+    link: null,
+  },
+  {
+    title: "SpreadsheetSync",
+    description:
+      "Add custom abilities to ClickFunnels 2.0 + Google Spreadsheets in magical ways.",
+    image: "/comingsoon.png",
+    link: null,
+  },
+];
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              app/page.js
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+    <main className="min-h-screen bg-gradient-to-br from-black via-gray-800 to-gray-900 text-white">
+      <div className="container mx-auto px-4 py-16">
+        <div className="flex flex-col items-center mb-16">
+          <Image
+            src="/logo.png?height=150&width=300"
+            alt="Company Logo"
+            width={300}
+            height={150}
+            className="mb-12"
+          />
+          <h1 className="text-4xl md:text-6xl lg:text-7xl font-bold text-center mb-3 animate-gradient">
+            Solve big problems
+            <br /> with small tools
+          </h1>
+          <h4 className="text-sm text-gray-600 md:text-xl px-6 w-2/3 mx-auto font-light text-center mb-8 animate-gradient">
+            <strong className="text-gray-500">Who are we?</strong> We are a
+            collective of passionate developers, designers &amp; entrepreneurs
+            building software products to enhance your marketing experience.
+          </h4>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+
+        <div className="space-y-12">
+          {products.map((product, index) => (
+            <div
+              key={index}
+              className="bg-gray-800/50 rounded-lg border border-gray-700 backdrop-blur-lg text-white hover:bg-gray-700/50 transition-colors duration-300"
+            >
+              <div className="p-8">
+                <div className="flex flex-col items-center md:flex-row gap-8">
+                  <Image
+                    src={product.image || "/placeholder.svg"}
+                    alt={product.title}
+                    width={400}
+                    height={300}
+                    className="rounded-lg mb-4 md:mb-0 md:w-1/2 object-cover"
+                  />
+                  <div className="md:w-1/2">
+                    <h2 className="text-3xl font-semibold mb-4">
+                      {product.title}
+                    </h2>
+                    <p className="text-lg mb-6">{product.description}</p>
+                    {product.link && (
+                      <Link
+                        href={product.link}
+                        target="_blank"
+                        className="inline-block bg-gradient-to-r from-purple-500 to-pink-500 text-white font-bold py-3 px-6 rounded-lg hover:from-purple-600 hover:to-pink-600 transition-colors duration-300"
+                      >
+                        Learn more
+                      </Link>
+                    )}
+                  </div>
+                </div>
+              </div>
+            </div>
+          ))}
+        </div>
+
+        <footer className="mt-12 mb-6 text-sm text-gray-500 text-center">
+          <p>&copy; 2025 - a FightClub App</p>
+        </footer>
+      </div>
+    </main>
   );
 }
